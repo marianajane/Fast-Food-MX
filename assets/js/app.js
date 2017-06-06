462c40da7295eada958959f2fb750e0605f8bab1
marianajane <mariana.musm@gmail.com>
2017-06-05 20:10:47 -0500

    Avance versión 3

--- assets/js/app.js
+++ assets/js/app.js
@@ -34,7 +34,7 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 // Búsqueda Restaurantes
 
-var restaurantes = [
+var restaurante = [
   
   {
     "nombre": "Chilakillers",
@@ -69,43 +69,37 @@ var restaurantes = [
   }
 ];
 
-var plantillaContacto = '<article class="row contact">' +
-        '<div class="card-panel hoverable grey lighten-5 z-depth-1">' +
-          '<div class="row valign-wrapper">' +
-            '<div class="col s3">' +
-              '<img src="__foto__" alt="Contact" class="circle responsive-img">' +
-            '</div>' +
-            '<div class="col s9">' +
-              '<h5 class="name">__nombre__</h5>' +
-              '<span class="black-text">' +
-                'Phone: __numero__' +
-              '</span>' +
-            '</div>' +
-          '</div>' +
-        '</div>' +
-  '</article>';
+var plantillaContacto = '<ul class="collection" id="restaurantes">'+
+        '<li class="collection-item avatar">'+
+          '<img src="assets/img/Icon_food.png" alt="" class="circle">'+
+          '<span class="title">__nombre__</span>'+
+          '<p>__giro__<br>'+
+             '__direccion__'+
+          '</p>'+
+          '<a href="#!" class="secondary-content"><i class="material-icons">grade</i></a>'+
+        '</li>';
+
 
 var cargarPagina = function () {
-  $("#search-form").submit(filtrarContactos);
+  $("#search-form").submit(filtrarRestaurantes);
 };
 
-var filtrarContactos = function (e) {
+var filtrarRestaurantes = function (e) {
   e.preventDefault();
   var criterioBusqueda = $("#search").val().toLowerCase();
-  var contactosFiltrados = contactos.filter(function (contacto) {
-    return contacto.nombre.toLowerCase().indexOf(criterioBusqueda) >= 0;
+  var restaurantesFiltrados = restaurante.filter(function (restaurante) {
+    return restaurante.nombre.toLowerCase().indexOf(criterioBusqueda) >= 0;
   });
-  mostrarContactos(contactosFiltrados);
+  mostrarRestaurantes(restaurantesFiltrados);
 };
 
-var mostrarContactos = function (contactos) {
+var mostrarRestaurantes = function (restaurante) {
   var plantillaFinal = "";
-  contactos.forEach(function (contacto) {
-    plantillaFinal += plantillaContacto.replace("__nombre__", contacto.nombre)
-      .replace("__numero__", contacto.numero)
-      .replace("__foto__", contacto.foto);
+  restaurante.forEach(function (restaurante) {
+    plantillaFinal += plantillaContacto.replace("__nombre__", restaurante.nombre);
+    
   });
-  $(".contacts").html(plantillaFinal);
+  $("#restaurantes").html(plantillaFinal);
 };
 
 $(document).ready(cargarPagina);
